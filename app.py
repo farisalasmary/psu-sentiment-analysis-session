90b5fe500578bd2baedef4568996cfcf9b8fcb6a
Faris Abdullah Alasmary <farisalasmary@gmail.com>
2021-10-19 00:33:44 +0300

    Inittial code!

---
+++ app.py
@@ -0,0 +1,116 @@
+import aranorm2 as aranorm
+
+import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+from sklearn.svm import SVC
+from sklearn.neural_network import MLPClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.naive_bayes import MultinomialNB, GaussianNB
+
+import pickle
+
+from flask import Flask, flash, request, redirect, url_for
+from werkzeug.utils import secure_filename
+
+app = Flask(__name__)
+
+##############################################################################################################
+#logistic_reg = pickle.load(open(f'{models_folder}/logistic_reg.pkl', 'rb'))
+#svm = pickle.load(open(f'{models_folder}/svm.pkl', 'rb'))
+#rf = pickle.load(open(f'{models_folder}/rf.pkl', 'rb'))
+#mlp = pickle.load(open(f'{models_folder}/mlp.pkl', 'rb'))
+#neu_logistic_reg = pickle.load(open(f'{models_folder}/neu_logistic_reg.pkl', 'rb'))
+#neu_mnb = pickle.load(open(f'{models_folder}/neu_mnb.pkl', 'rb'))
+#neu_rf = pickle.load(open(f'{models_folder}/neu_rf.pkl', 'rb'))
+#neu_mlp = pickle.load(open(f'{models_folder}/neu_mlp.pkl', 'rb'))
+
+models_folder = './models/'
+models_folder = models_folder.rstrip('/')
+
+vectorizer = pickle.load(open(f'{models_folder}/vectorizer.pkl', 'rb'))
+mnb = pickle.load(open(f'{models_folder}/mnb.pkl', 'rb'))
+neu_vectorizer = pickle.load(open(f'{models_folder}/neu_vectorizer.pkl', 'rb'))
+neu_svm = pickle.load(open(f'{models_folder}/neu_svm.pkl', 'rb'))
+
+
+##############################################################################################################
+
+def predict_multi_level(X, neu_vectorizer, neu_clf, vectorizer, clf):
+    return clf.predict(vectorizer.transform(X))
+    neu_y_pred = neu_clf.predict(neu_vectorizer.transform(X))
+    if len(X[neu_y_pred == 'NonNeutral']) > 0:
+        y_pred = clf.predict(vectorizer.transform(X[neu_y_pred == 'NonNeutral'])) # classify non neutral into positive or negative
+        neu_y_pred[neu_y_pred == 'NonNeutral'] = y_pred
+    
+    final_y_pred = neu_y_pred
+    return final_y_pred
+
+##############################################################################################################
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'input_text' not in request.form:
+            flash('No text found!')
+            return redirect(request.url)
+
+        text = request.form['input_text']
+        text = aranorm.normalize_arabic_text(text)
+        
+        if text == '':
+            return 'Please, write an Arabic sentance. Symbols and non-Arabic characters will be removed from the text....'
+                
+        predcited_sentiment = predict_multi_level(np.array([text]), neu_vectorizer, neu_svm, vectorizer, mnb)
+        predcited_sentiment = str(predcited_sentiment.squeeze())
+        
+        print(f'text: {text}')
+        print("Predicted Sentiment:", predcited_sentiment)
+        return predcited_sentiment
+    
+    return '''<!doctype html>
+<title>Sentiment Analysis</title>
+<script>
+function myFunction()
+{
+    // clear the output text box from the text
+    output_text_box = document.getElementById('output_text');
+    output_text_box.innerHTML = '';
+   
+    var elements = document.getElementsByClassName("formVal");
+    var formData = new FormData(); 
+    
+    for(var i=0; i<elements.length; i++)
+    {
+        formData.append(elements[i].name, elements[i].value);
+    }
+    var xmlHttp = new XMLHttpRequest();
+        xmlHttp.onreadystatechange = function()
+        {
+            if(xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            {
+                response = xmlHttp.responseText;
+                output_text_box = document.getElementById('output_text');
+                console.log(response);
+                output_text_box.innerHTML = response;
+            }
+        }
+        xmlHttp.open("post", "/"); 
+        xmlHttp.send(formData); 
+}
+</script>
+<h1>تحليل المشاعر من التغريدات</h1>
+<p>مثال: أحب أمي و أبي</p>
+<form method=post enctype=multipart/form-data>
+  <textarea id="input_text"class='formVal' rows="5" cols="50" type="text" name="input_text" placeholder="التغريدة"></textarea> <br>
+  <textarea id="output_text" class='formVal' rows="5" cols="50" type="text" name="output_text" placeholder="المشاعر المتوقعة"></textarea>
+  <input type="submit" value="submit_now" onclick="myFunction(); return false;">
+</form>
+
+</html>
+    '''
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
---
+++ aranorm2.py
@@ -0,0 +1,275 @@
+#!/usr/bin/python
+# -*- coding=utf-8 -*-
+
+"""
+    @author
+          ______         _                  _
+         |  ____|       (_)           /\   | |
+         | |__ __ _ _ __ _ ___       /  \  | | __ _ ___ _ __ ___   __ _ _ __ _   _
+         |  __/ _` | '__| / __|     / /\ \ | |/ _` / __| '_ ` _ \ / _` | '__| | | |
+         | | | (_| | |  | \__ \    / ____ \| | (_| \__ \ | | | | | (_| | |  | |_| |
+         |_|  \__,_|_|  |_|___/   /_/    \_\_|\__,_|___/_| |_| |_|\__,_|_|   \__, |
+                                                                              __/ |
+                                                                             |___/
+            Email: farisalasmary@gmail.com
+            Date:  Sep 26, 2021
+"""
+
+"""
+This work is mostly based on this work:
+https://github.com/MTG/ArabicTransliterator/blob/master/qalsadi/libqutrub/arabic_const.py
+and
+https://github.com/linuxscout/pyarabic/blob/master/pyarabic/araby_const.py
+"""
+
+
+import re
+import string
+from farasa.stemmer import FarasaStemmer
+
+
+COMMA = u'\u060C'
+SEMICOLON = u'\u061B'
+QUESTION = u'\u061F'
+HAMZA = u'\u0621'
+ALEF_MADDA = u'\u0622'
+ALEF_HAMZA_ABOVE = u'\u0623'
+WAW_HAMZA = u'\u0624'
+ALEF_HAMZA_BELOW = u'\u0625'
+YEH_HAMZA = u'\u0626'
+ALEF = u'\u0627'
+BEH = u'\u0628'
+TEH_MARBUTA = u'\u0629'
+TEH = u'\u062a'
+THEH = u'\u062b'
+JEEM = u'\u062c'
+HAH = u'\u062d'
+KHAH = u'\u062e'
+DAL = u'\u062f'
+THAL = u'\u0630'
+REH = u'\u0631'
+ZAIN = u'\u0632'
+SEEN = u'\u0633'
+SHEEN = u'\u0634'
+SAD = u'\u0635'
+DAD = u'\u0636'
+TAH = u'\u0637'
+ZAH = u'\u0638'
+AIN = u'\u0639'
+GHAIN = u'\u063a'
+TATWEEL = u'\u0640'
+FEH = u'\u0641'
+QAF = u'\u0642'
+KAF = u'\u0643'
+LAM = u'\u0644'
+MEEM = u'\u0645'
+NOON = u'\u0646'
+HEH = u'\u0647'
+WAW = u'\u0648'
+ALEF_MAKSURA = u'\u0649'
+YEH = u'\u064a'
+MADDA_ABOVE = u'\u0653'
+HAMZA_ABOVE = u'\u0654'
+HAMZA_BELOW = u'\u0655'
+ZERO = u'\u0660'
+ONE = u'\u0661'
+TWO = u'\u0662'
+THREE = u'\u0663'
+FOUR = u'\u0664'
+FIVE = u'\u0665'
+SIX = u'\u0666'
+SEVEN = u'\u0667'
+EIGHT = u'\u0668'
+NINE = u'\u0669'
+PERCENT = u'\u066a'
+DECIMAL = u'\u066b'
+THOUSANDS = u'\u066c'
+STAR = u'\u066d'
+MINI_ALEF = u'\u0670'
+ALEF_WASLA = u'\u0671'
+FULL_STOP = u'\u06d4'
+BYTE_ORDER_MARK = u'\ufeff'
+
+# Diacritics
+FATHATAN = u'\u064b'
+DAMMATAN = u'\u064c'
+KASRATAN = u'\u064d'
+FATHA = u'\u064e'
+DAMMA = u'\u064f'
+KASRA = u'\u0650'
+SHADDA = u'\u0651'
+SUKUN = u'\u0652'
+
+#Ligatures
+LAM_ALEF = u'\ufefb'
+LAM_ALEF_HAMZA_ABOVE = u'\ufef7'
+LAM_ALEF_HAMZA_BELOW = u'\ufef9'
+LAM_ALEF_MADDA_ABOVE = u'\ufef5'
+SIMPLE_LAM_ALEF = u'\u0644\u0627'
+SIMPLE_LAM_ALEF_HAMZA_ABOVE = u'\u0644\u0623'
+SIMPLE_LAM_ALEF_HAMZA_BELOW = u'\u0644\u0625'
+SIMPLE_LAM_ALEF_MADDA_ABOVE = u'\u0644\u0622'
+
+
+HARAKAT_PAT = re.compile(u"["+u"".join([FATHATAN, DAMMATAN, KASRATAN,
+                                        FATHA, DAMMA, KASRA, SUKUN,
+                                        SHADDA])+u"]")
+HAMZAT_PAT = re.compile(u"["+u"".join([WAW_HAMZA, YEH_HAMZA])+u"]")
+ALEFAT_PAT = re.compile(u"["+u"".join([ALEF_MADDA, ALEF_HAMZA_ABOVE,
+                                       ALEF_HAMZA_BELOW, HAMZA_ABOVE,
+                                       HAMZA_BELOW])+u"]")
+LAMALEFAT_PAT = re.compile(u"["+u"".join([LAM_ALEF,
+                                          LAM_ALEF_HAMZA_ABOVE,
+                                          LAM_ALEF_HAMZA_BELOW,
+LAM_ALEF_MADDA_ABOVE])+u"]")
+
+
+""" https://github.com/cltk/cltk/blob/master/cltk/corpus/arabic/alphabet.py """
+WESTERN_ARABIC_NUMERALS = ['0','1','2','3','4','5','6','7','8','9']
+
+#EASTERN_ARABIC_NUMERALS = [u'\u06F0', u'\u06F1', u'\u06F2', u'\u06F3', u'\u0664', u'\u06F5', u'\u0666', u'\u06F7', u'\u06F8', u'\u06F9']
+EASTERN_ARABIC_NUMERALS = [u'۰', u'۱', u'۲', u'۳', u'٤', u'۵', u'٦', u'۷', u'۸', u'۹']
+
+eastern_to_western_numerals = {}
+for i in range(len(EASTERN_ARABIC_NUMERALS)):
+    eastern_to_western_numerals[EASTERN_ARABIC_NUMERALS[i]] = WESTERN_ARABIC_NUMERALS[i]
+
+# Punctuation marks
+COMMA = u'\u060C'
+SEMICOLON = u'\u061B'
+QUESTION = u'\u061F'
+
+# Other symbols
+PERCENT = u'\u066a'
+DECIMAL = u'\u066b'
+THOUSANDS = u'\u066c'
+STAR = u'\u066d'
+FULL_STOP = u'\u06d4'
+MULITIPLICATION_SIGN = u'\u00D7'
+DIVISION_SIGN = u'\u00F7'
+
+arabic_punctuations = COMMA + SEMICOLON + QUESTION + PERCENT + DECIMAL + THOUSANDS + STAR + FULL_STOP + MULITIPLICATION_SIGN + DIVISION_SIGN
+all_punctuations = string.punctuation + arabic_punctuations + '()[]{}'
+
+all_punctuations = ''.join(list(set(all_punctuations)))
+
+
+
+stemmer = FarasaStemmer(interactive=True)
+
+
+def strip_tashkeel(text):
+    text = HARAKAT_PAT.sub('', text)
+    text = re.sub(u"[\u064E]", "", text,  flags=re.UNICODE) # fattha
+    text = re.sub(u"[\u0671]", "", text,  flags=re.UNICODE) # waSla
+    return text 
+
+
+def strip_tatweel(text):
+    return re.sub(u'[%s]' % TATWEEL, '', text)
+
+
+def remove_non_arabic(text):
+    return ' '.join(re.sub(u"[^\u0621-\u063A\u0640-\u0652 ]", " ", text,  flags=re.UNICODE).split())
+
+
+def keep_arabic_english_n_symbols(text):
+    return ' '.join(re.sub(u"[^\u0621-\u063A\u0640-\u0652a-zA-Z#@_:/ ]", " ", text,  flags=re.UNICODE).split())
+
+
+def normalize_hamza(text):
+    text = ALEFAT_PAT.sub(ALEF, text)
+    return HAMZAT_PAT.sub(HAMZA, text)
+
+
+def normalize_spellerrors(text):
+    text = re.sub(u'[%s]' % TEH_MARBUTA, HEH, text)
+    return re.sub(u'[%s]' % ALEF_MAKSURA, YEH, text)
+
+
+def normalize_lamalef(text):
+    return LAMALEFAT_PAT.sub(u'%s%s'%(LAM, ALEF), text)
+
+
+def normalize_arabic_text(text):
+    text = remove_non_arabic(text)
+    text = strip_tashkeel(text)
+    text = strip_tatweel(text)
+    
+    text = stemmer.stem(text)
+    
+    text = normalize_lamalef(text)
+    text = normalize_hamza(text)
+    text = normalize_spellerrors(text)
+    return text
+
+
+def remove_underscore(text):
+    return ' '.join(text.split('_'))
+
+
+def remove_retweet_tag(text):
+    return re.compile('\#').sub('', re.compile('rt @[a-zA-Z0-9_]+:|@[a-zA-Z0-9_]+').sub('', text).strip())
+
+
+def replace_emails(text):
+    emails = re.findall(r'[\w\.-]+@[\w\.-]+', text)
+    for email in emails:
+        text = text.replace(email,' يوجدايميل ')
+        #text = text.replace(email,' hasEmailAddress ')
+    return text
+
+def replace_urls(text):
+    return re.sub(r"http\S+|www.\S+", " يوجدرابط ", text)
+    #return re.sub(r"http\S+|www.\S+", " hasURL ", text)
+
+def convert_eastern_to_western_numerals(text):
+    for num in EASTERN_ARABIC_NUMERALS:
+        text = text.replace(num, eastern_to_western_numerals[num])
+    return text
+
+def remove_all_punctuations(text):
+    for punctuation in all_punctuations:
+        text = text.replace(punctuation, ' ')
+    return text
+
+def remove_emojis(text):
+    emoji_pattern = re.compile("["
+        u"\U0001F600-\U0001F64F"  # emoticons
+        u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+        u"\U0001F680-\U0001F6FF"  # transport & map symbols
+        u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                           "]+", flags=re.UNICODE)
+    return emoji_pattern.sub(r'', text)
+
+def replace_phone_numbers(text):
+    return re.sub(r'\d{10}', ' يوجدرقمهاتف ', text)
+    # return re.sub(r'\d{10}', ' hasPhoneNumber ', text)
+
+def remove_extra_spaces(text):
+    return ' '.join(text.split())
+
+'''
+very important note:
+    The order of the execution of the these function is extremely crucial.
+'''
+def normalize_tweet(text):
+    new_text = text.lower()
+#    new_text = stemmer.stem(new_text)
+    new_text = normalize_hamza(new_text)
+    new_text = strip_tashkeel(new_text)
+    new_text = strip_tatweel(new_text)
+    new_text = normalize_lamalef(new_text)
+    new_text = normalize_spellerrors(new_text)
+    new_text = remove_retweet_tag(new_text)
+    new_text = replace_emails(new_text)
+    new_text = remove_underscore(new_text)
+    new_text = replace_phone_numbers(new_text)
+    new_text = remove_all_punctuations(new_text)
+    new_text = replace_urls(new_text)
+    new_text = convert_eastern_to_western_numerals(new_text)
+#    new_text = keep_arabic_english_n_symbols(new_text)
+    new_text = remove_non_arabic(new_text)
+    new_text = remove_extra_spaces(new_text)
+    
+    return new_text
